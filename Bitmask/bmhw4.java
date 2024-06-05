21cd98443e97e1ebd5fedc6ac02bab1db94e26de
Ayush Singh <ayushsingh2009ddm@gmail.com>
2024-06-05 23:02:13 +0530

    OK

--- Bitmask/bmhw4.java
+++ Bitmask/bmhw4.java
@@ -16,13 +16,29 @@ public class bmhw4{
         System.out.println(sb);
     }
 
-
-
-
+    public static void binaryToDecimal(String b) {
+        int decimal = 0;
+        for (int i = 0; i < b.length(); i++) {
+            if (b.charAt(i) == '1') {
+                decimal += Math.pow(2, b.length() - i - 1);
+            }
+        }
+        System.out.println(decimal);
+    }
 
     public static void main(String[] args){
+        System.out.println("Press 1 to convert decimal to binary");
+        System.out.println("Press 2 to convert binary to decimal");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        decimalToBinary(n);
+        int choice = sc.nextInt();
+        if(choice == 1){
+            System.out.println("Enter a decimal number");
+            int n = sc.nextInt();
+            decimalToBinary(n);
+        }else if(choice == 2){
+            System.out.println("Enter a binary number");
+            String b = sc.next();
+            binaryToDecimal(b);
+        }
     }
 }
