407c8a7c2197f6b8ef9dd1fbbf2bb8f9b1a7f9e8
Ayush Singh <ayushsingh2009ddm@gmail.com>
2024-06-05 23:05:10 +0530

    Ok

--- Bitmask/Bm1.java
+++ Bitmask/Bm1.java
@@ -2,12 +2,12 @@ public class Bm1 {
    public static void main(String args[]) {
       int n = 7; //0111
       int pos = 2;
-      int bitMask = 0<<pos; // 0100
+      int bitMask = 1<<pos; // 0100
 
-      // int New = ~(bitMask); // 1011
+      int New = ~(bitMask); // 1011
 
 
-      // int newNumber = New & n;
-      System.out.println(bitMask);
+      int newNumber = New & n;
+      System.out.println(newNumber);
    }
 }
