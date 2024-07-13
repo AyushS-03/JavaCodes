2c1308bf714d9a590e324af51138b0e6e478248f
Ayush Singh <ayushsingh2009ddm@gmail.com>
2024-07-13 15:01:23 +0530

    hi

--- Basics/two.java
+++ Basics/two.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 
 public class two {
     public static void odd(int n) {
+        int d = 0;
         for(int i = 0; i <= n; i++){
             if(i % 2 != 0){
+                d = d + i;
                 System.out.print(i + " ");
             }
         }
+        System.out.println("\nSum of all odd no. are: "+d);
     }
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
+        int d;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         odd(n);
